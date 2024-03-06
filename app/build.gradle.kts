6e702eaa826ac942e0742553824428e95124e8ae
PSPDFKit <>
2024-03-06 11:54:30 +0000

    Update example to PSPDFKit for Android 2024.1.2

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+/*
+ *   Copyright © 2018-2024 PSPDFKit GmbH. All rights reserved.
+ *
+ *   The PSPDFKit Sample applications are licensed with a modified BSD license.
+ *   Please see License for details. This notice may not be removed from this file.
+ */
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    namespace = "com.pspdfkit.example.customfonts"
+    compileSdk = libs.versions.targetSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = namespace
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+
+        versionName = "2024.1.2"
+        versionCode = 138349
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = libs.versions.jvmTarget.get()
+    }
+    lint {
+        warningsAsErrors = true
+    }
+}
+
+dependencies {
+
+    // PSPDFKit is integrated from the PSPDFKit Maven repository. See the `repositories` block at the beginning
+    // of this file, which shows how to set up the repository in your app.
+    implementation("com.pspdfkit:pspdfkit:2024.1.2")
+}
