ec9db9750de9db4ab0e3cad99e707b40f961b961
Nutrient <>
2025-03-19 08:51:47 +0000

    Update example to Nutrient for Android 10.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         minSdk = 21
         targetSdk = compileSdk
 
-        versionName = "10.1.0"
-        versionCode = 142256
+        versionName = "10.1.1"
+        versionCode = 142264
     }
 
     compileOptions {
@@ -43,5 +43,5 @@ dependencies {
 
     // Nutrient is integrated from the Nutrient Maven repository. See the `repositories` block at the beginning
     // of this file, which shows how to set up the repository in your app.
-    implementation("io.nutrient:nutrient:10.1.0")
+    implementation("io.nutrient:nutrient:10.1.1")
 }
