e95c9bad84fb6f3c20c6180391cf26af17df6ef8
Nutrient <>
2026-08-06 11:41:27 +0000

    Update example to Nutrient for Android 11.6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         minSdk = 24
         targetSdk = compileSdk
 
-        versionName = "11.6.1"
-        versionCode = 148578
+        versionName = "11.6.2"
+        versionCode = 148596
     }
 
     compileOptions {
@@ -45,5 +45,5 @@ android {
 dependencies {
 
     // Nutrient is published to Maven Central, so no custom repository setup is required.
-    implementation("io.nutrient:nutrient-android-sdk:11.6.1")
+    implementation("io.nutrient:nutrient-android-sdk:11.6.2")
 }
